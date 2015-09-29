369b86ef153e226051f4f4926953998405656c8a
msgonzalez <msgonzalez@esei.uvigo.es>
2015-09-29 10:02:01 +0200

    Uploading psd file and some views

---
+++ www/index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<!-- Author -->
+  	<meta name="author" content="Marco Santana & Dario Mendez">
+  	<!-- Charset html4 -->
+  	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  	<!-- Charset html5 -->
+  	<meta charset="UTF-8">
+    <!-- mobile-optimized -->
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1">  Allow zoom on mobile device -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" /> <!-- Doesn't allow zoom on mobile devicess -->
+  	<!-- For IE(Crazy users) -->
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<!-- CSS(Bootstrap) -->
+  	<link href="Resources/css/bootstrap.css" rel="stylesheet">
+    <!-- CSS(Ours) -->
+    <link href="Resources/css/styles.css" rel="stylesheet">
+  	<title>Rogo |- Ask and respond </title>
+  </head>
+  <body class="bodyNoise">
+    <div class="container-fluid">
+
+      <div class="row headContainer">
+        <!-- BUSCAR + LOGO -->
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <div class="row">
+            <!-- BUSCAR -->
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+              <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search for...." aria-describedby="basic-addon2">
+                <span class="input-group-addon" id="basic-addon2">Buscar</span>
+              </div>
+            </div>
+            <!-- USUARIO -->
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+              <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                  <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                  <span>Username</span>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                  <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+                  <span>Configuracion</span>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                  <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+                  <span>Logout</span>
+                </div>
+              </div>
+            </div>
+            <!-- LOGO -->
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+              LOGO
+            </div>
+          </div>
+        </div>
+
+        <!-- REGISTRO + CAROUSEL -->
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <div class="row">
+            <!-- LOGIN/REGISTRO -->
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <div class="row">
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 registroContainer">
+                    REGISTRO
+                    <br>
+                    <br>
+                        <input type="text" class="form-control" placeholder="Nombre" aria-describedby="basic-addon2">
+                        <br>
+                        <input type="text" class="form-control" placeholder="Email" aria-describedby="basic-addon2">
+                        <br>
+                        <input type="text" class="form-control" placeholder="Alias" aria-describedby="basic-addon2">
+                        <br>
+                        <input type="text" class="form-control" placeholder="Password" aria-describedby="basic-addon2">
+                        <br>
+                        <input type="button" class="form-control" value="Registrarse" style="width:30%;">
+                  </div>
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 registroContainer">
+                    LOGIN
+                  </div>
+                </div>
+            </div>
+            <!-- CAROUSEL -->
+            <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 carouselContainer">
+              CAROUSEL
+              <BR>
+              CAROUSEL
+            </div>
+          </div>
+        </div>
+        <!-- SEPARADOR -->
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 separator">
+        </div>
+        <!-- CONTENIDO -->
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        </div>
+
+      </div>
+
+  </body>
+</html>
