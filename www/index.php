8d0b55790661111e87852206eaa6a837629fd195
msgonzalez <msgonzalez@esei.uvigo.es>
2015-09-30 14:01:43 +0200

    Added new view

--- www/index.php
+++ www/index.php
@@ -20,185 +20,216 @@
   </head>
   <body class="bodyNoise">
     <div class="container-fluid">
-
-      <div class="row headContainer">
-        <!-- BUSCAR + LOGO -->
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <div class="row">
-            <!-- BUSCAR -->
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-              <div class="input-group">
-                <input type="text" class="form-control" placeholder="Search for...">
-                <span class="input-group-btn">
-                  <button class="btn btn-default" type="button">Buscar</button>
-                </span>
+     <div class="row">
+        <!-- CABECERA -->
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 headContainer headerNoise">
+          <!-- BUSCAR + LOGO -->
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="row">
+              <!-- LOGO -->
+              <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 text-center visible-sm visible-xs">
+                <img src="Resources/images/logo1.png" alt="...">
               </div>
-            </div>
-            <!-- USUARIO -->
-            <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 iconsContainer">
-              <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 singleIconContainer">
-                  <span class="glyphicon glyphicon-user lightBlue" aria-hidden="true"></span>
-                  <span>Username</span>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 singleIconContainer">
-                  <span class="glyphicon glyphicon-cog lightBlue" aria-hidden="true"></span>
-                  <span>Configuracion</span>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 singleIconContainer">
-                  <span class="glyphicon glyphicon-off lightBlue" aria-hidden="true"></span>
-                  <span>Logout</span>
+              <!-- BUSCAR -->
+              <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <div class="input-group">
+                  <input type="text" class="form-control" placeholder="Search for...">
+                  <span class="input-group-btn">
+                    <button class="btn btn-default" type="button">Buscar</button>
+                  </span>
                 </div>
               </div>
-            </div>
-            <!-- LOGO -->
-            <div class="circle">
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 logoContainer text-center">
-              <img src="Resources/images/logo1.png" alt="...">
-            </div>
-          </div>
-        </div>
-
-        <!-- REGISTRO + CAROUSEL -->
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-          <div class="row">
-            <!-- LOGIN/REGISTRO -->
-            <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+              <!-- USUARIO -->
+              <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 indexIconsContainer">
                 <div class="row">
-                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 registroContainer">
-                    REGISTRO
-                    <br>
-                    <br>
-                    <form class="form-horizontal">
-                      <div class="form-group">
-                        <div class="col-lg-8 col-lg-offset-2">
-                          <input type="text" class="form-control" id="inputEmail" placeholder="Nombre">
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <div class="col-lg-8 col-lg-offset-2">
-                          <input type="text" class="form-control" id="inputEmail" placeholder="Email">
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <div class="col-lg-8 col-lg-offset-2">
-                          <input type="text" class="form-control" id="inputEmail" placeholder="Alias">
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <div class="col-lg-8 col-lg-offset-2">
-                          <input type="text" class="form-control" id="inputEmail" placeholder="Password">
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <div class="col-lg-8 col-lg-offset-2">
-                          <button type="submit" class="btn btn-primary">Registrarse</button>
-                        </div>
-                      </div>
-                    </form>
+                  <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 singleIconContainer">
+                    <span class="glyphicon glyphicon-user lightBlue" aria-hidden="true"></span>
+                    <span>Username</span>
                   </div>
-                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 registroContainer">
-                    LOGIN
-                    <br>
-                    <br>
-                    <form class="form-horizontal">
-                      <div class="form-group">
-                        <div class="col-lg-8 col-lg-offset-2">
-                          <input type="text" class="form-control" id="inputEmail" placeholder="Usuario">
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <div class="col-lg-8 col-lg-offset-2">
-                          <input type="text" class="form-control" id="inputEmail" placeholder="Password">
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <div class="col-lg-8 col-lg-offset-2">
-                          <button type="submit" class="btn btn-primary">Login</button>
-                        </div>
-                      </div>
-                    </form>
+                  <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 singleIconContainer">
+                    <span class="glyphicon glyphicon-cog lightBlue" aria-hidden="true"></span>
+                    <span>Configuracion</span>
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 singleIconContainer">
+                    <span class="glyphicon glyphicon-off lightBlue" aria-hidden="true"></span>
+                    <span>Logout</span>
                   </div>
                 </div>
-            </div>
-            <!-- CAROUSEL -->
-            <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 carouselContainer">
-
-              <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                  <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-                </ol>
-
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner" role="listbox">
-                  <div class="item active">
-                    <img class="carouselImageContainer" src="Resources/images/carouselExample.png" alt="...">
-                    <div class="carousel-caption">
-                      <h1>Categoria</h1>
-                      <p>Pregunta1</p>
-                      <a class="btn btn-lg btn-primary" href="#news1" role="button">Responder &raquo;</a>
-                    </div>
-                  </div>
-
-                  <div class="item">
-                    <img class="carouselImageContainer" src="Resources/images/carouselExample.png" alt="...">
-                    <div class="carousel-caption">
-                      <h1>Categoria</h1>
-                      <p>Pregunta2</p>
-                      <a class="btn btn-lg btn-primary" href="#news1" role="button">Responder &raquo;</a>
-                    </div>
-                  </div>
-
-                  <div class="item">
-                    <img class="carouselImageContainer" src="Resources/images/carouselExample.png" alt="...">
-                    <div class="carousel-caption">
-                      <h1>Categoria</h1>
-                      <p>Pregunta3</p>
-                      <a class="btn btn-lg btn-primary" href="#news1" role="button">Responder &raquo;</a>
-                    </div>
-                  </div>
-
-                  <div class="item">
-                    <img class="carouselImageContainer" src="Resources/images/carouselExample.png" alt="...">
-                    <div class="carousel-caption">
-                      <h1>Categoria</h1>
-                      <p>Pregunta4</p>
-                      <a class="btn btn-lg btn-primary" href="#news1" role="button">Responder &raquo;</a>
-                    </div>
-                  </div>
-
-                </div>
-
-                <!-- Controls -->
-                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                  <span class="glyphicon glyphicon-chevron-left lightBlue" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                  <span class="glyphicon glyphicon-chevron-right lightBlue" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
               </div>
+              <!-- LOGO -->
+              <div class="indexCircle visible-lg visible-md">
+              </div>
+              <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 indexLogoContainer text-center visible-lg visible-md">
+                <img src="Resources/images/logo1.png" alt="...">
+              </div>
+            </div>
+          </div>
 
+          <!-- REGISTRO + CAROUSEL -->
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="row">
+              <!-- LOGIN/REGISTRO -->
+              <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                  <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 registroContainer">
+                      REGISTRO
+                      <br>
+                      <br>
+                      <form class="form-horizontal">
+                        <div class="form-group">
+                          <div class="col-lg-8 col-lg-offset-2">
+                            <input type="text" class="form-control" id="inputEmail" placeholder="Nombre">
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="col-lg-8 col-lg-offset-2">
+                            <input type="text" class="form-control" id="inputEmail" placeholder="Email">
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="col-lg-8 col-lg-offset-2">
+                            <input type="text" class="form-control" id="inputEmail" placeholder="Alias">
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="col-lg-8 col-lg-offset-2">
+                            <input type="text" class="form-control" id="inputEmail" placeholder="Password">
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="col-lg-8 col-lg-offset-2">
+                            <button type="submit" class="btn btn-primary">Registrarse</button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 registroContainer">
+                      LOGIN
+                      <br>
+                      <br>
+                      <form class="form-horizontal">
+                        <div class="form-group">
+                          <div class="col-lg-8 col-lg-offset-2">
+                            <input type="text" class="form-control" id="inputEmail" placeholder="Usuario">
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="col-lg-8 col-lg-offset-2">
+                            <input type="text" class="form-control" id="inputEmail" placeholder="Password">
+                          </div>
+                        </div>
+                        <div class="form-group">
+                          <div class="col-lg-8 col-lg-offset-2">
+                            <button type="submit" class="btn btn-primary">Login</button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+              </div>
+              <!-- CAROUSEL -->
+              <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 carouselContainer">
+
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                  <!-- Indicators -->
+                  <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                  </ol>
+
+                  <!-- Wrapper for slides -->
+                  <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                      <img class="carouselImageContainer" src="Resources/images/carouselExample.png" alt="...">
+                      <div class="carousel-caption">
+                        <h1>Categoria</h1>
+                        <p>Pregunta1</p>
+                        <a class="btn btn-lg btn-primary" href="#news1" role="button">Responder &raquo;</a>
+                      </div>
+                    </div>
+
+                    <div class="item">
+                      <img class="carouselImageContainer" src="Resources/images/carouselExample.png" alt="...">
+                      <div class="carousel-caption">
+                        <h1>Categoria</h1>
+                        <p>Pregunta2</p>
+                        <a class="btn btn-lg btn-primary" href="#news1" role="button">Responder &raquo;</a>
+                      </div>
+                    </div>
+
+                    <div class="item">
+                      <img class="carouselImageContainer" src="Resources/images/carouselExample.png" alt="...">
+                      <div class="carousel-caption">
+                        <h1>Categoria</h1>
+                        <p>Pregunta3</p>
+                        <a class="btn btn-lg btn-primary" href="#news1" role="button">Responder &raquo;</a>
+                      </div>
+                    </div>
+
+                    <div class="item">
+                      <img class="carouselImageContainer" src="Resources/images/carouselExample.png" alt="...">
+                      <div class="carousel-caption">
+                        <h1>Categoria</h1>
+                        <p>Pregunta4</p>
+                        <a class="btn btn-lg btn-primary" href="#news1" role="button">Responder &raquo;</a>
+                      </div>
+                    </div>
+
+                  </div>
+
+                  <!-- Controls -->
+                  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left lightBlue" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right lightBlue" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+
+              </div>
             </div>
           </div>
         </div>
-
         <!-- SEPARADOR -->
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 separator">
         </div>
-
         <!-- CONTENIDO -->
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <BR>
+          <BR>
+          <BR>
+          <BR>
+            <BR>
+            <BR>
+            <BR>
+            <BR>
+              <BR>
+              <BR>
+              <BR>
+              <BR>
+                <BR>
+                <BR>
+                <BR>
+                <BR>
+                  <BR>
+                  <BR>
+                  <BR>
+                  <BR>
+                    <BR>
+                    <BR>
+                    <BR>
+                    <BR>
+                      <BR>
+                      <BR>
+                      <BR>
+                      <BR>
         </div>
-
       </div>
-
+    </div>
   </body>
   <!-- jQuery -->
       <script src="Resources/js/jquery.js"></script>
@@ -209,9 +240,6 @@
       <!-- Plugin JavaScript -->
       <script src="Resources/js/jquery.easing.min.js"></script>
 
-      <script src="Resources/js/loader.js"></script>
-
       <!-- One -->
       <script src="Resources/js/bootstrap.min.one.js"></script>
-      <script src="Resources/js/scripts.one.js"></script>
 </html>
